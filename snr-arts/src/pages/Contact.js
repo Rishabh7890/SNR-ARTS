f3802ade212c836a690f32d53440914bf696ecc3
rishabh <rishabh7890@gmail.com>
2019-07-11 21:00:28 -0400

    navbar shows when directly in page but does not read as import for some reason

--- snr-arts/src/pages/Contact.js
+++ snr-arts/src/pages/Contact.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 import myNavbar from '../components/myNavbar';
 
 class Contact extends Component {
@@ -7,7 +12,18 @@ class Contact extends Component {
   render() {
     return (
       <React.Fragment>
-        <myNavbar />
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">SNR Arts</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/paintings">Paintings</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
       </React.Fragment>
     );
   }
--- snr-arts/src/pages/Home.js
+++ snr-arts/src/pages/Home.js
@@ -1,5 +1,11 @@
-import React, { Component } from 'react';
-import myNavbar from '../components/myNavbar';
+import React, { Component } from "react";
+// import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+// import myNavbar from "../components/myNavbar";
 
 class Home extends Component {
   state = {};
@@ -7,11 +13,21 @@ class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <p>This is rendering</p>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">SNR Arts</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/paintings">Paintings</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
       </React.Fragment>
     );
   }
 }
-
 
 export default Home;
