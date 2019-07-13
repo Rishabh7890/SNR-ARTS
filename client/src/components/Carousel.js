dea4d136954ab2e7fa71e3ea9f7961428b7bcb91
rishabh <rishabh7890@gmail.com>
2019-07-13 17:47:17 -0400

    Carousel works

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <div className="container">
+  <Carousel autoPlay>
+    <div>
+      <img src="https://i.ibb.co/C8KQQhJ/mom-Painting1.jpg" />
+      <p className="legend">Inner Peace Abstract</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/0DXWYs1/mom-Painting2.jpg" />
+      <p className="legend">Lord Ganesha Green Theme</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/PDZWbDt/mom-Painting3.jpg" />
+      <p className="legend">Lord Krishna</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/5K0mY5x/mom-Painting4.jpg" />
+      <p className="legend">Desert Journey</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/djBf3F6/mom-Painting5.jpg" />
+      <p className="legend">Mystical Clouds</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/HF7Hb7j/mom-Painting6.jpg" />
+      <p className="legend">Night City Skyline</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/VH9JJC6/mom-Painting7.jpg" />
+      <p className="legend">Sitar Maiden</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/fDdjR5P/mom-Painting8.jpg" />
+      <p className="legend">Yoga</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/4Vs2X9s/mom-Painting9.jpg" />
+      <p className="legend">Lord Jesus</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/MNrM6FH/mom-Same-Image10.jpg" />
+      <p className="legend">Mystical Spiral</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/kh7nGgN/mom-Same-Image11.jpg" />
+      <p className="legend">Lord Ganesha Orange Theme</p>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/6mZCLjP/mom-Same-Image12.jpg" />
+      <p className="legend">Lord Ganesha In Nature</p>
+    </div>
+  </Carousel>
+
+  </div>
+);
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,9 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import Jumbotron from '../components/Jumbotron';
+import { render } from "react-dom";
+import Carousel from "../components/Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import myNavbar from "../components/myNavbar";
 
 class Home extends Component {
@@ -13,48 +16,47 @@ class Home extends Component {
     searchTerm: ''
   };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
+  // THIS IS THE CODE FOR INPUT CHANGE AND FORM SUBMIT IN THE SEARCH BAR. NEED TO DECIDE IF IM GOING TO USE MONGO OR JUST DO A JSON IN THE FRONT END
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
-    searchPaintings(this.state.searchTerm)
-    .then(({ data: { items: paintingList } }) => {
-      const paintingListCleaned = paintingList.map(painting => {
-        return {
-          paintingID: painting.id,
-          title: painting.title,
-          description: painting.description,
-          image: painting.image
-        }
-      })
-      return this.setState({ paintingList: paintingListCleaned });
-    })
-    .then(this.retrievePainting)
-    .catch(err => console.log(err));
-  };
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+
+  //   searchPaintings(this.state.searchTerm)
+  //   .then(({ data: { items: paintingList } }) => {
+  //     const paintingListCleaned = paintingList.map(painting => {
+  //       return {
+  //         paintingID: painting.id,
+  //         title: painting.title,
+  //         description: painting.description,
+  //         image: painting.image
+  //       }
+  //     })
+  //     return this.setState({ paintingList: paintingListCleaned });
+  //   })
+  //   .then(this.retrievePainting)
+  //   .catch(err => console.log(err));
+  // };
 
   render() {
     return (
       <React.Fragment>
-        <Navbar bg="primary" variant="dark">
+        <Navbar bg="light" variant="light">
           <Navbar.Brand href="#home">SNR Arts</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/paintings">Paintings</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search Paintings" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
         </Navbar>
         <Jumbotron fluid bg={'dark'} color={'light'} pageTitle={'Welcome to SNR Arts'} />
+        <Carousel />
       </React.Fragment>
     );
   }
