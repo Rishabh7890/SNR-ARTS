b4fa550aae7cd8dc58c5595a57adbc05170015de
rishabh <rishabh7890@gmail.com>
2019-07-15 17:48:24 -0400

    same navbar and footer in all pages

--- client/src/pages/Paintings.js
+++ client/src/pages/Paintings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Footer from "../components/Footer";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
@@ -11,18 +12,16 @@ class Paintings extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar bg="primary" variant="dark">
+        <Navbar className="navCustom d-flex justify-space-between" bg="light" variant="light">
           <Navbar.Brand href="/">SNR Arts</Navbar.Brand>
-          <Nav className="mr-auto">
+          <Nav className="ml-auto navCust">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/paintings">Paintings</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search Paintings" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
         </Navbar>
+
+        <Footer children={'Website Created By Rishabh Goel. All Works Of Art Created By Nishi Goel Copyright 2019'} />
       </React.Fragment>
     );
   }
