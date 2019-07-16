d451f3f2b3819f91f54a09fdba73947763bf9771
rishabh <rishabh7890@gmail.com>
2019-07-16 19:29:12 -0400

    added some structure to paintingIngo may/may not work

---
+++ client/src/pages/paintingInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PaintingInfo extends React.Component {
+
+  render() {
+    let selectedPaintingId = localStorage.getItem("selectedPainting");
+
+    return (
+      <div>
+        {/* card details here  */}
+      </div>
+    )
+  }
+}
+
+export default PaintingInfo;
