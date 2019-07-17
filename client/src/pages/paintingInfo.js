9d1a5455a604a16a17d146bb81b670042041fb1c
rishabh <rishabh7890@gmail.com>
2019-07-16 20:29:37 -0400

    need to figure out how to get specific painting details on info page

--- client/src/pages/paintingInfo.js
+++ client/src/pages/paintingInfo.js
@@ -8,6 +8,7 @@ class PaintingInfo extends Component {
   state = {};
 
   render() {
+    let selectedPaintingId = localStorage.getItem("selectedPainting");
     return (
       <React.Fragment>
         <Navbar className="navCustom d-flex justify-space-between" bg="light" variant="light">
@@ -18,6 +19,9 @@ class PaintingInfo extends Component {
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar>
+
+       
+
 
         <Footer children={'Website Created By Rishabh Goel. All Works Of Art Created By Nishi Goel Copyright 2019'} />
       </React.Fragment>
