7a38121d778b04b1dc64dca2b31fd770339949fc
rishabh <rishabh7890@gmail.com>
2019-07-18 22:58:39 -0400

    card not defined error

--- client/src/pages/PaintingInfo.js
+++ client/src/pages/PaintingInfo.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "../App.css";
+import cardInfo from "../paintings.json";
 
 class PaintingInfo extends React.Component {
 
   state={
-    cardInfo: localStorage.getItem('selectedPainting')
+    card: localStorage.getItem('selectedPainting')
  }
  
  render(){
+   console.log(card);
     return(
       <div>
-         <img src={this.state.cardInfo.image}
+         <img src={require(this.state.card.image)}
               alt="image"
               // className= "img-fluid img-thumbnail rounded bg-dark"
          />
